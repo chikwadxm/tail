385a0c9ae3646383f06c907336f8834d7cb1d6db
chikwadxm <joshjohn2085@gmail.com>
2025-09-26 13:11:54 +0100

    tail

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  theme: {
+    extend: {
+      screens: {
+        'xs':{'min': '340px', 'max': '640'},
+        '3xl': '1920px', // Extra extra large devices
+      }
+    }
+  }
+}
